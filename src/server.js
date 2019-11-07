a9c7d8f02f947bc1cb73d7922a8984bd3d31711b
igor <isola@lbto.org>
2019-11-07 15:44:11 -0700

    moment test in server.js node

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const FormData = require('form-data');
+const moment = require('moment-timezone');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,20 +11,10 @@ var form = {'name': "igor", 'cat': 'Copi'};
 const formData = new FormData(form);
 //formData.append('name', "igor");
 //formData.append('cat', 'Copi');
-
+/* good
 const instance = axios.create({
   baseURL: 'http://localhost/cgi-bin'
 });
-/*axios({
-    url: 'http://localhost/cgi-bin/axios.pl',
-    method: 'post',
-//    data: formData,
-//    config: { headers: {'Content-Type': 'multipart/form-data' }}
-    data: {
-      foo: 'bar'
-    }
-  }).then();
-  */
 
 instance.post("/axios.pl", {
       foo: 'bar'
@@ -31,4 +22,36 @@ instance.post("/axios.pl", {
 
 instance.post("/axios.pl", 
 formData).then();
+*/
+
+console.log(moment.locale());
+
+console.log(moment("2019-11-04T16:21:00").tz("America/Phoenix").format());
+
+console.log(new Date("2019-11-04T16:21:00Z") - new Date("2019-11-04T09:21:00-07:00"))
+
+//console.log(moment.tz("2019-11-04T16:21:00", "UTC"), moment.tz("2019-11-04T09:21:00", "America/Toronto"));
+
+console.log(new Date(moment.tz("2019-11-04T16:21:00", "UTC")), new Date(moment.tz("2019-11-04T09:21:00", "America/Phoenix")));
+
+
+console.log(moment("2019-11-04T16:21:00").tz("UTC") - moment("2019-11-04T09:21:00").tz("America/Toronto"));
+
+Date.prototype.addHours = function(h) {
+  this.setTime(this.getTime() + (h*60*60*1000));
+  return this;
+}
+console.log(new Date(), new Date().addHours(80));
+
+
+console.log(new Date(moment.tz("2019-11-04T09:21:00", "America/Phoenix").add(11.4, 'hours')), 
+moment.tz("2019-11-04T18:21:00-07:00", "America/Phoenix").add(11.4, 'hours').format(),
+moment.tz("2019-11-04T18:21:00", "America/Phoenix").add(11.42, 'hours').format(),
+moment("2019-11-04T18:21:00-07:00").add(11.42, 'hours').format()
+);
+
+const nightEnd = moment("2019-11-04T18:21:00-07:00").add(11.42, 'hours');
+
+console.log(nightEnd.toDate() < new Date());
+console.log(nightEnd.toDate());
 
